750e7db1d26149d20513de228e78a2417d3c9a93
Mal <133685339+maloudnee@users.noreply.github.com>
2025-09-27 02:05:07 -0400

    update detectors.js

---
+++ detectors.js
@@ -0,0 +1,63 @@
+// Grammar check
+export async function checkGrammar(text) {
+    if(!text) return null;
+
+    const res = await fetch("https://api.languagetool.org/v2/check", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded"},
+        body: `text=${encodedURIComponent(text)}&language=en-US`
+    });
+
+    const data = await res.json();
+    if (data.matches && data.matches.length > 0) {
+        return { type: "grammar", error: data.matches[0].message };
+    }
+    return null;
+}
+
+// Code check
+export function checkCode(text, element) {
+    if(!text || !element) return null;
+    if(element.tagName === "TEXTAREA" && element.classList.contains("code-editor")){
+        if(text.includes("System.out.println") && !text.trim().endsWith(";")) {
+            return { type: "code", error: "mising semi colon" };
+        }
+    }
+    return null;
+}
+
+// Math check
+export function checkMath(element) {
+    if (!text || !element) return null; 
+    if(element.classList.contains("incorrect") || element.innerText.includes("Incorrect")) {
+        return { type: "math", error: "wrong answer" };
+    }
+    return null;
+}
+
+// Quiz/Assessment check
+export function checkQuiz(element) {
+    const wrongMsgs = document.querySelectorAll(".incorrect, .feedback, .error");
+    for (const msg of wrongMsgs) {
+        if(/incorrect|wrong/i.test(msg.innerText)) {
+            return { type: "quiz", error: "wrong answer" };
+        }
+    }
+}
+
+// Master detection
+export async function detectMistake(text, element) {
+    const gammarMistake = await checkGrammar(text);
+    if(grammarMistake) return grammarMistake;
+
+    const codeMistake = await checkCode(text, element);
+    if(codeMistake) return codeMistake;
+
+    const mathMistake = await checkMath(element);
+    if(mathMistake) return mathMistake;
+
+    const quizMistake = await checkQuiz(element);
+    if(quizMistake) return quizMistake; 
+
+    return null; 
+}
